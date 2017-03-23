622aec45727460c7882729cb9f3c684c89d0d2d3
samelie <sam@stinkdigital.com>
2017-03-22 20:39:07 -0700

    Still a loose directory after delete

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 require('dotenv').config()
 var path = require('path')
 var fs = require('fs')
+var readDir = require('readdir')
 var watch = require('node-watch');
 const exec = require('child_process').exec
 
 const SUPPORTED_FORMATS = [".mov", ".mpeg4", ".mp4", ".avi", ".wmv", ".mpegps", ".flv", ".3gpp", ".webm", ".mkv"]
 
 function uploadVideo(filePath) {
-  const { PRIVACY, CLIENT_SECRETS, DELETE_AFTER_UPLOAD } = process.env;
-  const { name, base } = path.parse(filePath)
-  const cmd = `youtube-upload --title="${name}" --privacy=${PRIVACY} --credentials-file=credentials.json --client-secrets=${CLIENT_SECRETS} "${filePath}"`
-
-  exec(cmd, function(err, stdout, stderr) {
-
-    //a video id
-    if(stdout.indexOf(" ") < 0){
-      if(Boolean(DELETE_AFTER_UPLOAD)){
-        fs.unlinkSync(filePath)
+  return new Promise((yes, no) => {
+    const { PRIVACY, CLIENT_SECRETS, DELETE_AFTER_UPLOAD } = process.env;
+    const { name, base } = path.parse(filePath)
+    const cmd = `youtube-upload --title="${name}" --privacy=${PRIVACY} --credentials-file=credentials.json --client-secrets=${CLIENT_SECRETS} "${filePath}"`
+    console.log(cmd);
+    exec(cmd, function(err, stdout, stderr) {
+      console.log(`youtube id ${stdout}`);
+      //a video id
+      if (stdout.indexOf(" ") < 0) {
+        yes(filePath)
+      } else {
+        no(filePath)
       }
-    }
-  });
-
+    });
+  })
 }
 
 watch(process.env.DOWNLOAD_DIR, { recursive: true }, function(evt, name) {
@@ -28,8 +30,33 @@ watch(process.env.DOWNLOAD_DIR, { recursive: true }, function(evt, name) {
     case 'created':
     case 'update':
       const ext = path.parse(name).ext;
-      if (SUPPORTED_FORMATS.indexOf(ext) > -1) {
+      const containerFolder = fs.lstatSync(name).isDirectory() ? name : null
+      if (SUPPORTED_FORMATS.indexOf(ext) > -1 && !fs.lstatSync(name).isDirectory()) {
+        console.log(name);
         uploadVideo(name)
+          .then(filePath => {
+            const {dir} = path.parse(filePath)
+            const { DELETE_AFTER_UPLOAD } = process.env;
+            if (Boolean(DELETE_AFTER_UPLOAD)) {
+              const fileToDelete = containerFolder || filePath
+              exec(`rm -rf "${fileToDelete}"`, function(err, stdout, stderr) {
+                console.log(`Deleted ${fileToDelete}`);
+                if(containerFolder){
+                  fs.rmdir(containerFolder)
+                  console.log(`Deleted directory${fileToDelete}`);
+                }
+              })
+            }
+          })
+          .catch(err => {
+
+          })
+          /*if (fs.lstatSync(name).isDirectory()) {
+            const files = readDir.readSync( name, SUPPORTED_FORMATS.map(p=>(`**${p}`)), readDir.ABSOLUTE_PATHS );
+            console.log(files);
+          } else {
+          }*/
+
       }
       break;
   }
